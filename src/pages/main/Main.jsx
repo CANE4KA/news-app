4a1a98842bec3a19e333bbe599430c0568802912
CANE4KA <145180300+CANE4KA@users.noreply.github.com>
2024-10-11 17:42:00 +0500

    Add filter by categories

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 
+import { Categories } from '../../components/categories/Categories'
 import { NewsBanner } from '../../components/news-banner/NewsBanner'
 import { NewsList } from '../../components/news-list/NewsList'
 import { Pagination } from '../../components/pagination/Pagination'
@@ -7,10 +8,12 @@ import { Skeleton } from '../../components/skeleton/Skeleton'
 
 import styles from './main.module.css'
 
-import { getNews } from '../../api/apiNews'
+import { getCategories, getNews } from '../../api/apiNews'
 
 export const Main = () => {
 	const [news, setNews] = useState([])
+	const [categories, setCategories] = useState([])
+	const [selectedCategory, setSelectedCategory] = useState('All')
 	const [isLoading, setIsLoading] = useState(true)
 	const [currentPage, setCurrentPage] = useState(1)
 	const totalPages = 10
@@ -19,7 +22,11 @@ export const Main = () => {
 	const fetchNews = async currentPage => {
 		try {
 			setIsLoading(true)
-			const response = await getNews(currentPage, pageSize)
+			const response = await getNews({
+				page_number: currentPage,
+				page_size: pageSize,
+				category: selectedCategory === 'All' ? null : selectedCategory
+			})
 			setNews(response.news)
 			setIsLoading(false)
 		} catch (error) {
@@ -27,9 +34,22 @@ export const Main = () => {
 		}
 	}
 
+	const fetchCategories = async () => {
+		try {
+			const response = await getCategories()
+			setCategories(['All', ...response.categories])
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchCategories()
+	}, [])
+
 	useEffect(() => {
 		fetchNews(currentPage)
-	}, [currentPage])
+	}, [currentPage, selectedCategory])
 
 	const handleNextPage = () => {
 		if (currentPage < totalPages) {
@@ -49,6 +69,12 @@ export const Main = () => {
 
 	return (
 		<main className={styles.main}>
+			<Categories
+				categories={categories}
+				setSelectedCategory={setSelectedCategory}
+				selectedCategory={selectedCategory}
+			/>
+
 			{isLoading ? (
 				<Skeleton type='banner' count={1} />
 			) : (
